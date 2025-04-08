b10ed523931d0d0478ad36f5bc1697ca95af2f88
Elif Kesen <elifkesen@gmail.com>
2025-04-08 21:56:27 +0200

    08.04 v3

--- src/test/java/pages/KiwiPage.java
+++ src/test/java/pages/KiwiPage.java
@@ -1,4 +1,30 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class KiwiPage {
+    public KiwiPage(){
+        PageFactory.initElements((WebDriver)Driver.getAndroidDriver(),this);
+    }
+    @FindBy(xpath = "(//*[@class='android.widget.Button'])[4]")
+    public WebElement ContinueAsButton;
+
+    @FindBy(xpath = "//*[@text='Return']")
+    public WebElement returnButton;
+
+    @FindBy(xpath = "//*[@text='One way']")
+    public WebElement OneWayButton;
+
+    @FindBy(xpath = "//*[@text='From:']")
+    public WebElement FromButton;
+
+    @FindBy(xpath = "//*[@content-desc='Clear All']")
+    public WebElement ClearButton;
+
+    @FindBy(xpath = "//*[@class='android.widget.EditText']")
+    public WebElement KalkisYeriYazmaKutusu;
 }
--- src/test/java/tests/day03/Kiwi.java
+++ src/test/java/tests/day03/Kiwi.java
@@ -2,21 +2,61 @@ package tests.day03;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.KiwiPage;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class Kiwi {
     AndroidDriver<AndroidElement>driver= Driver.getAndroidDriver();
 
+    KiwiPage page=new KiwiPage();
+
     @Test
-    public void Kiwi(){
+    public void Kiwi() throws InterruptedException {
         // uygulamanin yuklendigi dogrulanir
+        Assert.assertTrue(driver.isAppInstalled("com.skypicker.main"));
+
         //uygulamanin basariyla acildigi dogrulanir
+        Thread.sleep(2000);
+        Assert.assertTrue(page.ContinueAsButton.isDisplayed());
+
         // misafir olarak devam et e tiklanir
+        Thread.sleep(2000);
+        page.ContinueAsButton.click();
+
         // ardindan gelecek olan 3 adimda da yesil butona basilarak devam edilir
+        Thread.sleep(2000);
+        for (int i = 0; i < 3; i++) {
+            ReusableMethods.koordinatTiklamaMethodu(550,2050,300);
+        }
+
         // Trip type,one way olarak secilir
+        Thread.sleep(2000);
+        page.returnButton.click();
+        Thread.sleep(2000);
+        page.OneWayButton.click();
+
        // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+        Thread.sleep(2000);
+        page.FromButton.click();
+        page.ClearButton.click();
+
       // kalkis yapilacak sehir Ankara girilir ve sec e tiklanir
+        Thread.sleep(2000);
+        page.KalkisYeriYazmaKutusu.click();
+
+        if (driver.isKeyboardShown()){
+            driver.getKeyboard().pressKey("Ankara");
+        }else {
+            page.KalkisYeriYazmaKutusu.sendKeys("Ankara");
+        }
+
+
+
+
+
       // varis  secenegi Frankfurt secilir
       // gidis tarihi 23 Nisan olarak secilir ve set date e tiklanir
         // search butonuna tiklanir
